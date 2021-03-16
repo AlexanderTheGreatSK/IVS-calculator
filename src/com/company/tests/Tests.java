886da5c6febc1ce401be8cc202afce11b1833d43
Tedro <sktedro@gmail.com>
2021-03-16 11:04:21 +0100

    Ready to write the tests, now that I know how
    I've been trying to create test suites, but JUnit 5 does not support those. I will be using "Nested" tests instead

---
+++ src/com/company/tests/Tests.java
@@ -0,0 +1,90 @@
+package com.company.tests;
+
+import com.company.CalcLib;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/*
+Basic single: +, -, /, *
+Basic single edges:
+Advanced single: fact, pow, sqrt, mod
+Advanced single edges:
+
+Overflow single:
+Operator precedence:
+White characters impact:
+
+More complex calculations
+ */
+public class Tests {
+
+    @BeforeAll
+    static void beforeAll() {
+    }
+
+    @BeforeEach
+    void beforeEach() {
+    }
+
+    @AfterEach
+    void afterEach() {
+    }
+
+    @AfterAll
+    static void afterAll() {
+    }
+
+    @Nested
+    @DisplayName("Basic operations, single operator")
+    class BasicSingle {
+
+        @BeforeEach
+        void beforeEach() {
+        }
+
+        @AfterEach
+        void afterEach() {
+        }
+
+        @Test
+        @DisplayName("Addition")
+        void Addition() {
+            assertEquals(16, CalcLib.main("1+1"));
+        }
+
+        @Test
+        @DisplayName("Subtraction")
+        void Subtraction() {
+            assertEquals(16, CalcLib.main("1+1"));
+        }
+
+    }
+
+    @Nested
+    @DisplayName("Basic operations, single operator")
+    class AdvancedSingle {
+
+        @BeforeEach
+        void beforeEach() {
+        }
+
+        @AfterEach
+        void afterEach() {
+        }
+
+        @Test
+        @DisplayName("Multiplication")
+        void Multiplication() {
+            assertEquals(16, CalcLib.main("1*1"));
+        }
+
+        @Test
+        @DisplayName("Division")
+        void Division() {
+            assertEquals(16, CalcLib.main("1*1"));
+        }
+
+    }
+}
