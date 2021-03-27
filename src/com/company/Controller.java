7091b3afa2cc019228b5511bb3afb3f0ac4b93c8
AlexanderTheGreatSK <alex.okrucky@gmail.com>
2021-03-27 14:55:01 +0100

    init GUI

--- src/com/company/Controller.java
+++ src/com/company/Controller.java
@@ -1,2 +1,25 @@
-package com.company;public class Controller {
+package com.company;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
+
+public class Controller {
+
+    @FXML
+    private Text outputText;
+
+    @FXML
+    public void buttonPress(ActionEvent event) {
+        String value = ((Button) event.getSource()).getText();
+        outputText.setText(outputText.getText() + value);
+    }
+
+    @FXML
+    private void finishEq(ActionEvent event) {
+
+    }
+
 }
+
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,30 @@
 package com.company;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class Main  extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("UNI(x)calc");
+        primaryStage.setMaxHeight(445);
+        primaryStage.setMaxWidth(286);
+        primaryStage.setMinHeight(445);
+        primaryStage.setMinWidth(286);
+
+        //primaryStage.getIcons().add(new Image(Main.class.getResourceAsStream("logo.png")));
+
+        primaryStage.setScene(new Scene(root, 286, 417));
+        primaryStage.show();
+    }
 
     public static void main(String[] args) {
-	// write your code here
+        launch(args);
     }
 }
