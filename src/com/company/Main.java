9f999b7d6ff916e797d6a344ed19799d023f52e2
Tedro <sktedro@gmail.com>
2021-03-31 17:49:53 +0200

    Profiling script done. Now: find, download and set up a profiler.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
         //System.out.println(args[0]);
         //CalcLib.main(args[0]);
-        System.out.println(CalcLib.main("root2, 1)"));
+        System.out.println(CalcLib.main(args[0]));
     }
 }
