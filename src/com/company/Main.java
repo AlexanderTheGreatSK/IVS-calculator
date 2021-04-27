c22eb02f8297516a05d17ae5dcb0552ce0877523
AlexanderTheGreatSK <alex.okrucky@gmail.com>
2021-04-27 17:08:23 +0200

    documentation update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -61,8 +61,7 @@ public class Main extends Application {
     /**
      * The main() method is ignored in correctly deployed JavaFX application.
      * main() serves only as fallback in case the application can not be
-     * launched through deployment artifacts, e.g., in IDEs with limited FX
-     * support. NetBeans ignores main().
+     * launched through deployment artifacts
      *
      * @param args the command line arguments
      */
